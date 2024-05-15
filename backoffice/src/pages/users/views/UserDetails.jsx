814bfe8eae056950d1b4462eb29e656f3338bad6
Yasminadouani09 <jassiadouani09@gmail.com>
2024-05-15 01:56:06 +0100

    dashboard and some details done

--- backoffice/src/pages/users/views/UserDetails.jsx
+++ backoffice/src/pages/users/views/UserDetails.jsx
@@ -3,11 +3,19 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { fetchuser } from '../../../store/UserInfo';
 import { useNavigate, useParams } from "react-router-dom";
-import Card from '@mui/material/Card';
+import AspectRatio from "@mui/joy/AspectRatio";
+import Box from "@mui/joy/Box";
+import Divider from "@mui/joy/Divider";
+import FormControl from "@mui/joy/FormControl";
+import Card from "@mui/joy/Card";
+import CardActions from "@mui/joy/CardActions";
+import CardOverflow from "@mui/joy/CardOverflow";
+import { MdEdit } from "react-icons/md";
+import { Link } from "react-router-dom";
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, FormLabel } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { orange, cyan } from '@mui/material/colors';
 import Button from '@mui/material/Button';
@@ -27,77 +35,154 @@ useEffect(() => {
   dispatch(fetchuser(id));
 }, [dispatch]);
   return (
-    <div className="d-flex justify-content-center py-4">
-  <Card sx={{ maxWidth: 490 }}>
-      <CardActionArea>
-        
-        <CardMedia
-          component="img"
-          height="140"
-          image={user?.image}
-          alt="green iguana"
-        />
-       
-       <div className="d-flex justify-content-center py-1" >
-           <ThemeProvider theme={theme}>
-      <Button variant="contained">{user?.role}</Button>
-      </ThemeProvider>
+    <Box className=" d-flex justify-content-center align-items-center">
+      <Card style={{ width: "50rem" }}>
+        <Box sx={{ mb: 1 }}>
+          <div className="d-flex justify-content-center py-3 align-items-center flex-wrap">
+            <h1
+              style={{
+                fontFamily: "Brittany Signature",
+              }}
+            >
+              Personal
+            </h1>
+            <h1
+              style={{
+                fontFamily: "Brittany Signature",
+                color: "rgb(66, 177, 188)",
+              }}
+            >
+              Informations 🧑‍💻
+            </h1>
+            <div
+              style={{
+                height: "5px",
+                width: "80px",
+                backgroundColor: "rgb(66, 177, 188)",
+                marginRight: "42px",
+              }}
+            ></div>
+          </div>
+        </Box>
+        <Divider />
+
+        <div className="d-flex justify-content-around py-3 align-items-around">
+          <img
+            src={user?.image}
+            loading="lazy"
+            alt=""
+            style={{ width: "20rem", borderRadius: "100%" }}
+          />
         </div>
-        <div className="d-flex justify-content-center py-1">
-        <ThemeProvider theme={theme}>
-  
-  <Button variant="contained" color="secondary">{user?.email}</Button>
-  </ThemeProvider>
+
+        <div>
+          <div className="d-flex justify-content-around gap-5 px-5 align-items-around">
+            <FormControl>
+              <FormLabel>Nom</FormLabel>
+              <Typography size="sm">{user?.firstName} </Typography>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Prenom</FormLabel>
+              <Typography size="sm">{user?.lastName}</Typography>
+            </FormControl>
+          </div>
+          <div className="d-flex justify-content-around gap-5 px-5">
+            <FormControl>
+              <FormLabel>Role</FormLabel>
+              <Typography size="sm">{user?.role} </Typography>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Typography>{user?.email}</Typography>
+            </FormControl>
+          </div>
+          <div
+            className="d-flex justify-content-around gap-5 px-5"
+            spacing={50}
+          >
+            <FormControl>
+              <FormLabel>Adresse</FormLabel>
+              <Typography size="sm">{user?.address}</Typography>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Numero</FormLabel>
+              <Typography size="sm">{user?.phone}</Typography>
+            </FormControl>
+          </div>
         </div>
-   
-        <CardContent>
-          <Typography gutterBottom variant="h3" component="div">
-          {user?.firstName} {user?.lastName}
-          </Typography>
-      
-       
-          <Typography gutterBottom variant="h5" component="div">
-          Phone 
-          </Typography>
-          <Typography variant="body4" color="text.secondary">
-          {user?.phone} 
-          </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-          Address 
-          </Typography>
-          <Typography variant="body4" color="text.secondary">
-          {user?.address} 
-          </Typography>
+      </Card>
+    </Box>
 
-        </CardContent>
-      </CardActionArea>
-    </Card>
-{/* {[  
-     
-        'Dark',
-      ].map((variant, idx) => (
-        <Toast
-          className="d-inline-block m-1"
-          bg={variant.toLowerCase()}
-          key={idx}
-        >
-          <Toast.Header>
-            <img
-              src="holder.js/20x20?text=%20"
-              className="rounded me-2"
-              alt=""
-            />
-            <strong className="me-auto">{user?.firstName} {user?.lastName}</strong>
-            <small>{user?.role}</small>
-          </Toast.Header>
-          <Toast.Body className={variant === 'Dark' && 'text-white'}>
-          {user?.email}
+    //     <div className="d-flex justify-content-center py-4">
+    //   <Card sx={{ maxWidth: 490 }}>
+    //       <CardActionArea>
 
-          </Toast.Body>
-        </Toast>
-      ))}
- */}
+    //         <CardMedia
+    //           component="img"
+    //           height="140"
+    //           image={user?.image}
+    //           alt="green iguana"
+    //         />
 
-    </div>
-  )
+    //        <div className="d-flex justify-content-center py-1" >
+    //            <ThemeProvider theme={theme}>
+    //       <Button variant="contained">{user?.role}</Button>
+    //       </ThemeProvider>
+    //         </div>
+    //         <div className="d-flex justify-content-center py-1">
+    //         <ThemeProvider theme={theme}>
+
+    //   <Button variant="contained" color="secondary">{user?.email}</Button>
+    //   </ThemeProvider>
+    //         </div>
+
+    //         <CardContent>
+    //           <Typography gutterBottom variant="h3" component="div">
+    //           {user?.firstName} {user?.lastName}
+    //           </Typography>
+
+    //           <Typography gutterBottom variant="h5" component="div">
+    //           Phone
+    //           </Typography>
+    //           <Typography variant="body4" color="text.secondary">
+    //           {user?.phone}
+    //           </Typography>
+    //           <Typography gutterBottom variant="h5" component="div">
+    //           Address
+    //           </Typography>
+    //           <Typography variant="body4" color="text.secondary">
+    //           {user?.address}
+    //           </Typography>
+
+    //         </CardContent>
+    //       </CardActionArea>
+    //     </Card>
+    // {/* {[
+
+    //         'Dark',
+    //       ].map((variant, idx) => (
+    //         <Toast
+    //           className="d-inline-block m-1"
+    //           bg={variant.toLowerCase()}
+    //           key={idx}
+    //         >
+    //           <Toast.Header>
+    //             <img
+    //               src="holder.js/20x20?text=%20"
+    //               className="rounded me-2"
+    //               alt=""
+    //             />
+    //             <strong className="me-auto">{user?.firstName} {user?.lastName}</strong>
+    //             <small>{user?.role}</small>
+    //           </Toast.Header>
+    //           <Toast.Body className={variant === 'Dark' && 'text-white'}>
+    //           {user?.email}
+
+    //           </Toast.Body>
+    //         </Toast>
+    //       ))}
+    //  */}
+
+    //     </div>
+  );
 }
