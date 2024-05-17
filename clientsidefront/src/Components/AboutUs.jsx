09729c3d9d8112d5f191937c855b064a3ed5bce1
Yasminadouani09 <jassiadouani09@gmail.com>
2024-05-17 14:31:11 +0100

    dashboard

--- clientsidefront/src/Components/AboutUs.jsx
+++ clientsidefront/src/Components/AboutUs.jsx
@@ -14,13 +14,7 @@ export default function AboutUs() {
       <div className="d-flex justify-content-center p-5 flex-wrap align-items-center flex-wrap">
         <h1 style={{ fontFamily: "Brittany Signature" }}>About us</h1>
      
-        <Form onSubmit={handleSubmit}>
-          <input type="text" onChange={(e) => setPrompt(e.target.value)} />
-          <button className="btn btn-warning" type="submit">
-            ask
-          </button>
-          <p>{response}</p>
-        </Form>
+        
         <div
           style={{
             height: "5px",
